e4e063ca40a2c5c83ae8d66fd17238cf4bc80ef3
Nhat Toan <toanln@cyfeer.com>
2020-04-12 21:28:08 +0700

    add code generator

---
+++ plopfile.js
@@ -0,0 +1,41 @@
+module.exports = plop => {
+  plop.setGenerator('component', {
+    description: 'Create a component',
+    prompts: [
+      {
+        type: 'list',
+        name: 'package',
+        message: 'What is your package?',
+        choices: ['dashboard', 'web-app'],
+      },
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your component name?',
+        validate(value) {
+          if (value === '') {
+            return 'Please enter a valid component';
+          }
+          return true;
+        },
+      },
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: 'packages/{{package}}/src/containers/{{pascalCase name}}/index.js',
+        templateFile: 'plop-templates/component/component-index.js.hbs',
+      },
+      {
+        type: 'add',
+        path: 'packages/{{package}}/src/containers/{{pascalCase name}}/{{pascalCase name}}.js',
+        templateFile: 'plop-templates/component/component-view.js.hbs',
+      },
+      {
+        type: 'add',
+        path: 'packages/{{package}}/src/containers/{{pascalCase name}}/{{camelCase name}}Enhance.js',
+        templateFile: 'plop-templates/component/component-enhance.js.hbs',
+      },
+    ],
+  });
+};
